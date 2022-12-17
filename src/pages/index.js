ea892987ea8debaf0c0fc3261ec9149ba25fe204
Juan Hebert <jchable@xifralifestyle.com>
2022-12-16 17:01:02 -0700

    added changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,6 @@ export default function Home() {
                   whileInView="visible"
                 >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </motion.p>
-
               </div>
               <div className={styles.borderedContainer}>
                 <div className={styles.bg}>
