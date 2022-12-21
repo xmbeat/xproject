99828e607c27623f247127f1b69bd88e5601aada
Juan Hebert <jchable@xifralifestyle.com>
2022-12-21 14:56:59 -0600

    change position

--- src/components/threejs/scene.jsx
+++ src/components/threejs/scene.jsx
@@ -5,7 +5,7 @@ export default function Scene() {
 
     return <>
         <group rotation={[0, 0.2, 0]}>
-            <Wave position={[-20, 0, 0]} lineCount={20} width={40} waveCount={4} separation={0.2} />
+            <Wave position={[-20, -3, 0]} lineCount={20} width={50} waveCount={4} separation={0.2} />
         </group>
         <group rotation={[0.3, -0.2, -1]}>
             <Wave position={[-15, -3, 0]} lineCount={20} width={40} waveCount={4} separation={0.2} />
