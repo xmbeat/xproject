07e773768f1f517ac0ecc06adaa39d23653377ea
Dante Camacho <dcamacho@decentrauniverse.com>
2023-01-04 18:33:03 -0600

    Login Page Added

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,73 @@
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from './login.module.sass'
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { motion } from 'framer-motion'
+
+export default function Login() {
+
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = e =>{
+        e.preventDefault() 
+        // console.log('user: '+ user)
+        // console.log('password: '+ password)
+    }
+
+    return  <div className={styles.main}>
+                <div className={styles.bg}>
+                    <Image fill alt= '' src='/assets/images/city-login-dark.png' />
+                </div>
+                <motion.div  
+                className={styles.content}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1}}
+                transition={{ duration: 0.5 }}
+                >
+                    <div>
+                        <h1>
+                            <span>x </span>project
+                        </h1>
+                        <h2>
+                            Welcome to X project
+                        </h2>
+                    </div>
+
+                    <form onSubmit={onSubmit}>
+                        <div className={styles.formBox}>
+                            <div className={styles.inputBox}>
+                                <div>
+                                    <label>Username</label>
+                                    <input
+                                    placeholder='Enter your backoffice username' 
+                                    // type="text" 
+                                    name="username" 
+                                    autoComplete="off" 
+                                    required
+                                    value={user}
+                                    onChange = {e => setUser(e.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <label>Password</label>
+                                    <input
+                                    placeholder='Enter your backoffice password' 
+                                    type="password" 
+                                    name="password" 
+                                    autoComplete="off"
+                                    required
+                                    value={password}
+                                    onChange = {e => setPassword(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <button type='submit' className={styles.button}>
+                                Login
+                                <AiOutlineArrowRight />
+                            </button>
+                        </div>
+                    </form>
+                </motion.div>
+            </div>
+}
