9f8dda8e585a9e8073a01b68426b4203b428cc44
Christian Sanchez <csanchez@xifralifestyle.com>
2023-01-06 18:36:04 -0600

    HF: LangSelector error handling

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next";
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-    en: {
+    en: { 
         translation: {
             "connect-wallet": "Claim Plots",
             "connect": "Claim",
